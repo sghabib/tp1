92a8d0627c60fba9bc208aade79676a23e4bf575
Schreyer Michel <michelschreyer18@gmail.com>
2018-02-05 17:32:24 -0500

    CV_Schreyer_Michel

--- TP1_Cyr_Schreyer/src/Partie_1/CV.java
+++ TP1_Cyr_Schreyer/src/Partie_1/CV.java
@@ -1,29 +1,26 @@
 package Partie_1;
 
-import java.util.ArrayList;
-
 public class CV {
 
-	public static String nom;
-	public static String prenom;
-	public static String formation;
-	public static int nbAnnee;
-	public static ArrayList<CV> listCompetence = new ArrayList<CV>();
-	public static String Attente;
+	private static String nom;
+	private static String prenom;
+	private static String formation;
+	private static int nbAnnee;
+	private static String[] CompetenceMike = { "Programmeur", "Tester des programmes", "Modelisation" };
+	private static String Attente;
 
-	public static void main(String[] args) {
-		
-		System.out.println("Bienvenue chez Barette!");
-		afficher();
-		
+	public static void afficher(String nom, String prenom, String formation, int nbAnnee, String Attente) {
+
+		System.out.println("\nAffichage des CV:" + "\nNom: " + nom + "\nPrénom: " + prenom + "\nFormation: " + formation
+				+ "\nNombre d'année d'expérience: " + nbAnnee + "\nCompétences: " + CompetenceMike[0] + ", "
+				+ CompetenceMike[1] + ", " + CompetenceMike[2] + "\nAttente pour le cours 4B4: " + Attente);
+
 	}
 
-	public static void afficher() {
-		
-		System.out.println("\nAffichage des CV:" + "\nNom: " + nom + "\nPrénom: " + prenom + "\nFormation: " + formation
-				+ "\nNombre d'année d'expérience: " + nbAnnee + "\nCompétences: " + listCompetence.toString()
-				+ "\nAttente pour le cours 4B4: " + Attente);
-		 
+	public static void main(String[] args) {
+
+		System.out.println("Bienvenue chez Barette!");
+		afficher("Schreyer", "Michel", "DEC en programmation en préparation", 4, "Passer le cours");
 		
 	}
 
